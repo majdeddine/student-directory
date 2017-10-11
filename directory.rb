7cb429df4046fcd3c7d0cdbe5e78cef089f56635
majdeddine <majda-jambouri@hotmail.fr>
2017-10-11 13:22:57 +0100

    Add students hobbies, country and height

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+# add 2 variable for names that starts with specific letter and have specific length
+$first_letter = "D"
+$name_length = 12
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -8,7 +11,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobbies: "Write DRY code", country: :UK, height: "Over 1m"}
     puts "Now we have #{students.count} students"
     #get another name from user
     name = gets.chomp
@@ -23,11 +26,12 @@ def print_header
 end
 
 # rewrite the each methods using while loop
+
 def print(students)
   i=0
   while i < students.length
-    if students[i][:name][0] == "D" && students[1][:name].length < 12
-    puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    if students[i][:name][0] == $first_letter && students[1][:name].length < $name_length
+    puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort) (hobbie #{students[i][:hobbies]}) (country #{students[i][:country]}) (#{students[i][:height]} height)"
     end
     i+=1
   end
